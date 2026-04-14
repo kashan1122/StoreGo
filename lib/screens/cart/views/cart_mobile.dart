ce32e2e335e110d451ff5a8b798523543d062387
kashan1122 <muhammadkashan413@gmail.com>
2026-04-14 20:25:58 +0500

    clean push

--- lib/screens/cart/views/cart_mobile.dart
+++ lib/screens/cart/views/cart_mobile.dart
@@ -18,78 +18,42 @@ class CartMobile extends StatefulWidget {
 class _CartMobileState extends State<CartMobile> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Container(
-          // height: 120,
-          padding: EdgeInsets.all(2),
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              color: Colors.red,
-              gradient: const LinearGradient(
-                  colors: [Colors.blue, Colors.red])
-        ),
-          child: Stack(
-            alignment: Alignment.centerLeft,
-            children: [
-              Container(
-                height: 110,
-                decoration: BoxDecoration(
-                    gradient: const LinearGradient(colors: [
-                      Colors.black,
-                      Colors.green,
-                    ]),
-                    borderRadius: BorderRadius.circular(20)),
-              ),
-              Positioned(
-                // left: 15,
-                child: CustomPaint(
-                  size: const Size(300, 70), // width & height define oval shape
-                  painter: OvalPainter(),
-                ),
-              ),
-            ],
+    return CustomScaffold(
+      appBar: AppBar(
+        title: const CustomText("CART SCREEN"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        // mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const DeliveryAddressSection(),
+          SizedBox(height: context.hp(3)),
+          const CustomText(
+            ' Shopping List',
+            // style: AppTextStyle.title,
           ),
-        ),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  BlocBuilder<CartBloc, CartState>(builder: (context, state) {
+                    print("CART CONTEXT: $context");
+                    print("cart item: ${state.status}");
+                    if (state.status == CartStatus.loading) {
+                      return const Center(
+                          child: CircularProgressIndicator(
+                        color: Colors.black,
+                      ));
+                    }
+                    return const ShoppingListSection();
+                  }),
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
-    //   CustomScaffold(
-    //     appBar: AppBar(
-    //       title: const CustomText("CART SCREEN"),
-    //     ),
-    //     body: Column(
-    //       crossAxisAlignment: CrossAxisAlignment.start,
-    //       // mainAxisAlignment: MainAxisAlignment.start,
-    //       children: [
-    //         const DeliveryAddressSection(),
-    //         SizedBox(height: context.hp(3)),
-    //         const CustomText(
-    //           ' Shopping List',
-    //           // style: AppTextStyle.title,
-    //         ),
-    //         Expanded(
-    //           child:  SingleChildScrollView(
-    //             child: Column(
-    //               children: [
-    //                 BlocBuilder<CartBloc, CartState>(
-    //                     builder: (context, state) {
-    //                       print("CART CONTEXT: $context");
-    //                       print("cart item: ${state.status}");
-    //                       if (state.status == CartStatus.loading) {
-    //                         return const Center(child: CircularProgressIndicator(color: Colors.black,));
-    //                       }
-    //                       return  const ShoppingListSection();
-    //                     }
-    //                 ),
-    //               ],
-    //             ),
-    //           ),
-    //         )
-    //       ],
-    //     ),
-    // );
-    //
   }
 }
 
